ef1a7aca66aa6890e97256cba5d31698cdd85529
Kevnel <kevichen510@gmail.com>
2024-04-09 10:58:36 -0700

    Finished

--- MethodsPractice/Form1.cs
+++ MethodsPractice/Form1.cs
@@ -22,7 +22,10 @@ namespace MethodsPractice
             /// TODO - Place method calls here
 
             PrintSignature();
-
+            RectangleArea();
+            Addition();
+            CircleArea();
+            LargestValue();
         }
 
         /// 1. Print a signature to the screen
@@ -39,7 +42,13 @@ namespace MethodsPractice
 
         public void RectangleArea()
         {
+            int unitSquare, width, height;
 
+            width = 5;
+            height = 7;
+
+            unitSquare = width * height;
+            q2Output.Text = $"The area of a {width} x {height} rectangle is {unitSquare} units squared";
         }
 
         /// 3. Create a method to get numbers from q3Num1Input and q3Num2Input,
@@ -47,14 +56,41 @@ namespace MethodsPractice
         /// 
         /// 5 + 4 = 9 
 
+        public void Addition()
+        {
+            try
+            {
+                int first = Convert.ToInt32(q3Num1Input.Text);
+                int second = Convert.ToInt32(q3Num2Input.Text);
 
-        
+                int total = first * second;
+                q3Output.Text = $"{first} + {second} = {total}";
+            }
+            catch
+            {
+
+            }
+        }
+
         /// 4. Create a method to calculate the area of a circle based on  
         /// the radius entered in radiusInput, then display output as follows:
         /// 
         /// The area of a circle with radius of 4 is 50.24 units squared
 
+        public void CircleArea()
+        {
+            try
+            {
+                int radius = Convert.ToInt32(radiusInput.Text);
 
+                double area = radius * radius * 3.14159;
+                q4Output.Text = $"The area of a circle with radius of {radius} is {area.ToString(".##")} units squared";
+            }
+            catch
+            {
+
+            }
+        }
 
         /// 5. 3U Only - Create a method that will determine and display
         /// the maximum of 3 values entered into q5Num1Input, q5Num2Input,
@@ -62,7 +98,35 @@ namespace MethodsPractice
         /// 
         /// Inputs: 5, 7, 2
         /// he largest value is 7
+        public void LargestValue()
+        {
+            try
+            {
+                int third = Convert.ToInt32(q5Num1Input.Text);
+                int fourth = Convert.ToInt32(q5Num2Input.Text);
+                int fifth = Convert.ToInt32(q5Num3Input.Text);
 
+                if (third > fourth && third > fifth)
+                {
+                    q5Output.Text = $"The largest value is {third}";
+                }
+                else if (fourth > third && fourth > fifth)
+                {
+                    q5Output.Text = $"The largest value is {fourth}";
+                }
+                else if (fifth > third && fifth > fourth)
+                {
+                    q5Output.Text = $"The largest value is {fifth}";
+                }
+                else
+                {
+                    q5Output.Text = "All numbers are the same";
+                }
+            }
+            catch
+            {
 
+            }
+        }
     }
 }
